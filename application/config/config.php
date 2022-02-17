e6c8621fde3b797b4e142658b5195e288b20c9fe
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-02-17 07:45:33 +0700

    add view login

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$scheme = (isset($_SERVER["HTTPS"]) && @$_SERVER["HTTPS"] == "on") ? "https" : "http";
+$config['base_url'] = "$scheme://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +37,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin/admin_dashboard');
+		$this->load->view('login');
 	}
 }
