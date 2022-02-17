2b184de14dd36e82f2a391024e55ce788d87f455
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-02-17 13:38:06 +0700

    add menu eks int implementasi kerjasama

---
+++ application/views/admin/components/footer.php
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js">
+    </script>
+    <script src="<?= base_url(); ?>assets/js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+    <script src="<?= base_url(); ?>assets/assets/demo/chart-area-demo.js"></script>
+    <script src="<?= base_url(); ?>assets/assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest"></script>
+    <script src="<?= base_url(); ?>assets/js/datatables-simple-demo.js"></script>
