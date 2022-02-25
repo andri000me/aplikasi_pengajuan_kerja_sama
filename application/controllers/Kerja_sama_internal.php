06bf374f946f687fa7c1459a92496a3aa63e7164
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-02-25 19:07:10 +0700

    add filter kerja sama internal for admin

--- application/controllers/Kerja_sama_internal.php
+++ application/controllers/Kerja_sama_internal.php
@@ -26,6 +26,23 @@ class Kerja_sama_internal extends CI_Controller {
 		}
 	}
 
+	public function filter_admin($id_kategori_kerja_sama)
+	{
+
+		if ($this->session->userdata('logged_in') == true AND $this->session->userdata('id_user_level') == 1) {
+
+		$data['kerja_sama_internal'] = $this->m_kerja_sama_internal->get_kerja_sama_internal_by_kategori($id_kategori_kerja_sama);
+		$data['kerja_sama_internal_pengusul'] = $this->m_kerja_sama_internal->get_kerja_sama_internal_pengusul_by_kategori($id_kategori_kerja_sama)->result_array();
+		$data['kategori_kerja_sama'] = $this->m_kategori_kerja_sama->get_kategori_kerja_sama();
+		$data['status_kerja_sama'] = $this->m_status_kerja_sama->get_status_kerja_sama();
+		$data['user'] = $this->m_user->get_user();
+		$this->load->view('admin/view_kerja_sama_internal', $data);
+
+		}else{
+			$this->session->set_flashdata('loggin_err','loggin_err');
+			redirect('Login/index');
+		}
+	}
 	public function input_data_admin(){
 		if ($this->session->userdata('logged_in') == true AND $this->session->userdata('id_user_level') == 1) {
 
--- application/models/m_kerja_sama_internal.php
+++ application/models/m_kerja_sama_internal.php
@@ -24,6 +24,19 @@ class M_kerja_sama_internal extends CI_Model
         return $hasil;
     }
 
+    function get_kerja_sama_internal_by_kategori($id_kategori_kerja_sama){
+        $hasil=$this->db->query("SELECT * FROM kerja_sama_internal 
+        JOIN user ON kerja_sama_internal.id_lembaga_mitra = user.id
+        JOIN kategori_kerja_sama ON kerja_sama_internal.id_kategori_kerja_sama = kategori_kerja_sama.id_kategori_kerja_sama
+        JOIN status_kerja_sama ON kerja_sama_internal.id_status_kerja_sama = status_kerja_sama.id_status_kerja_sama WHERE kerja_sama_internal.id_kategori_kerja_sama='$id_kategori_kerja_sama'");
+        return $hasil;
+    }
+    function get_kerja_sama_internal_pengusul_by_kategori($id_kategori_kerja_sama){
+        $hasil=$this->db->query("SELECT nama_mitra as nama_pengusul FROM kerja_sama_internal 
+        JOIN user ON kerja_sama_internal.id_pengusul = user.id WHERE kerja_sama_internal.id_kategori_kerja_sama='$id_kategori_kerja_sama'");
+        return $hasil;
+    }
+
     function update_kerja_sama_internal($id, $no_usulan, $keterangan, $id_lembaga_mitra, $id_pengusul, $id_status_kerja_sama, $file_kerja_sama_internal, $id_kategori_kerja_sama){
         $hsl = $this->db->query("UPDATE kerja_sama_internal SET no_usulan='$no_usulan', keterangan='$keterangan' , id_lembaga_mitra='$id_lembaga_mitra', id_pengusul='$id_pengusul', id_status_kerja_sama='$id_status_kerja_sama' , file_kerja_sama_internal='$file_kerja_sama_internal', id_kategori_kerja_sama='$id_kategori_kerja_sama' WHERE id_kerja_sama_internal='$id'");
          return $hsl;
